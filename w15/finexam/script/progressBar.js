17fca70c79bc3b221f2a071dd252f05b2d52a4c8
TinyFatCat <126849634+TinyFatCat@users.noreply.github.com>
2023-06-16 07:39:52 +0900

    w15 재추가

---
+++ w15/finexam/script/progressBar.js
@@ -0,0 +1,13 @@
+const indicator = document.querySelector(".indicator");
+const progressTarget = document.querySelector("main");
+
+const progressBar = () => {
+  const progressTargetBCR = progressTarget.getBoundingClientRect();
+  const progress = 1 - progressTargetBCR.bottom / progressTargetBCR.height;
+  console.log(progress);
+  if (progress >= 0 && progress <= 1) {
+    indicator.style.cssText = "transform: scaleX(" + progress + ")";
+  }
+};
+
+document.addEventListener("scroll", progressBar);
